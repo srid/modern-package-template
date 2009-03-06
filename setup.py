a4be733ac9c73a9ab41968ac6c3003e856449d15
klein.stephane <devnull@localhost>
2009-03-06 16:40:52 +0000

    Improve package meta informations

--- setup.py
+++ setup.py
@@ -8,13 +8,40 @@ setup(name='harobed.paster_template.advanced_package',
       version=version,
       description="PasteScript template to create advanced setuptools-enabled package with buildout and namespace support",
       long_description="""\
+
+Features supports
+=================
+
+* namespace support
+* buildout support
+
+Install
+=======
+
+Install the template :
+
+::
+
+    $ sudo easy_install harobed.paster_template.advanced_package
+
+
+Using
+=====
+
+Create a new package with advanced_package template :
+
+::
+    
+    $ paster create -t advanced_package my.new.package package=my.new.package
+
+You can answer True to buildout question to install buildout file (bootstrap.py and buildout.cfg).
 """,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
-      author='KLEIN Stéphane',
+      author='KLEIN Stephane',
       author_email='stephane@harobed.org',
-      url='',
-      license='',
+      url='http://code.google.com/p/paste-script-advanced-package-template/',
+      license='GNU General Public License v3',
       packages = find_packages('src'),
       package_dir = {'':'src'},
       namespace_packages = ['harobed', 'harobed.paster_template'],
