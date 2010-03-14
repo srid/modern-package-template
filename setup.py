bf431de9664d23877765b0199c305d4699b836fa
srid <sridhar.ratna@gmail.com>
2010-03-14 13:44:14 -0700

    normalize version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
-version = '1.0b1'
+version = '1.0.0b1'
 
 setup(name='modern-package-template',
       version=version,
