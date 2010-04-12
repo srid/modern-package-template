ffca2a162d3e0ac3572405904832ab9493321ad5
srid <sridhar.ratna@gmail.com>
2010-04-11 17:57:59 -0700

    up ver

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
-version = '1.0'
+version = '1.0.1'
 
 
 setup(name='modern-package-template',
